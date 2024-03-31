6a5c31d997731d1c72e3ab6cff0f25908eb93dbd
Marcel-ORRJ <marcelo.rrosajr@gmail.com>
2024-03-31 12:30:09 -0300

    :bulb: Criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
-function styles() {
+function compileSass() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/styles/'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+function compressImages() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
 }
+
+exports.default = function() {
+    gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.series(compileSass));
+    gulp.watch('./src/images/**/*', { ignoreInitial: false }, gulp.series(compressImages));
+};
